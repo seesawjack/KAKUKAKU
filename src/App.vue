ebfc3254f1cc2bd46e1077bf8c03da09b8f4882e
seesawjack <seesawjack14@gmail.com>
2023-04-27 14:15:34 +0800

    202304271414-(1)安裝tailwindcss(2)將漢字標注平假名功能拆成元件

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,15 @@
 <script setup>
-import { ref } from 'vue';
-import { diff_match_patch }from 'diff-match-patch';
-import { useRequestStore } from './stores/request';
-import { useHiraganaStore } from './stores/hiragana';
-
-const reqStore = useRequestStore();
-const hiraganaStore = useHiraganaStore();
-
-const lyrics = ref('');
-const resultLyrics = hiraganaStore.resultLyrics; 
-async function generateHiraganaLyrics(){
-  const reqData = hiraganaStore.updateLyricsInput(lyrics.value.value.replace(/\n/g,'||'));
-  const hiraganaLyrics = await reqStore.request(JSON.stringify(reqData))
-  hiraganaStore.kanjiLabelHiragana(hiraganaLyrics.converted,lyrics.value.value)
-
-}
+import LyricsPage from './components/lyrics/LyricsPage.vue'
 
 </script>
 
 <template>
-  <div>
-    <div>
-      <label for="">歌詞</label>
-      <textarea name="" id="" cols="100" rows="10" ref="lyrics"></textarea>
-    </div>
-    <template v-if="resultLyrics.length">
-      <div v-html="resultLyrics.join('')"></div>
-    </template>
-    <button @click="generateHiraganaLyrics">點我</button>
-  </div>
+  
 </template>
 
-<style scoped>
+<style>
+body{
+  background: #2c2d44;
+  background-image: linear-gradient(45deg, #3f3251 2%, #002025 100%);
+}
 </style>

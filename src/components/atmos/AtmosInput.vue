0c51f571732eece9de7e21a1fe8c3b07575e49c0
seesawjack <seesawjack14@gmail.com>
2023-05-16 11:44:05 +0800

    202305161143-歌詞頁ui調整

--- src/components/atmos/AtmosInput.vue
+++ src/components/atmos/AtmosInput.vue
@@ -5,24 +5,34 @@
       class="resize-none bg-[transparent] border border-solid rounded-3xl py-2 px-5  w-full"
       type="text"
       :placeholder="inputPh"
-      v-model="searchValue"
+      v-model.lazy="searchValue"
     />
-    <search-glasses class="absolute right-3 top-2 opacity-40"/>
+    <search-glasses class="absolute right-3 top-2" :class="{'opacity-40':!iconStyle,'cursor-pointer':iconStyle}" @click="searchSong"/>
     </div>
   </form>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref,watch } from "vue";
 import SearchGlasses from '../svg/SearchGlasses.vue'
 const props = defineProps({
   inputPh: String,
 });
+
+const iconStyle = ref(false)
 const emit = defineEmits(["searchResult"]);
 
 const searchValue = ref("");
 
+watch(()=>searchValue.value,()=>{
+  if(searchValue.value){
+    iconStyle.value = true;
+    return;
+  }
+  iconStyle.value = false;
+})
 function searchSong() {
+  if(!iconStyle.value){return ;}
   emit("searchResult", searchValue.value);
 }
 </script>
--- src/components/atmos/AtmosLyric.vue
+++ src/components/atmos/AtmosLyric.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="mt-5 text-2xl text-left">
+  <div class="mt-5 text-xl text-left bg-slate-950/60 px-3 py-2 rounded-xl">
     <template v-for="lyric,index in lyrics" :key="lyric">
       <p class="tracking-[2px] before:border-t before:border-dashed" v-html="lyric"></p>
-      <div v-if="index !== lyrics.length-1" class="line border-t border-dashed h-px my-2"></div>
+      <div v-if="index !== lyrics.length-1" class="line border-t h-px my-4"></div>
     </template>
     </div>
 </template>
@@ -14,6 +14,8 @@ const props = defineProps({
 
 </script>
 
-<style>
-
+<style scoped>  
+.line{
+  border-style:inset;
+}
 </style>

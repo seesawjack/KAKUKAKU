da8d80a6c7048963c67df7184cfcc2887362234b
seesawjack <seesawjack14@gmail.com>
2023-06-02 12:28:04 +0800

    202306021227-歌詞上傳功能完成

--- src/components/atmos/AtmosEdit.vue
+++ src/components/atmos/AtmosEdit.vue
@@ -14,7 +14,20 @@
       >
         確定
       </button>
-      <button class="flex-1 border border-solid rounded-xl">上傳歌詞</button>
+      <label
+        class="flex-1 border border-solid rounded-xl leading-10 hover:bg-slate-600 cursor-pointer"
+        for="file-uploader"
+      >
+        <input
+          type="file"
+          id="file-uploader"
+          data-target="file-uploader"
+          accept=".txt,.doc,.docx"
+          class="hidden"
+          @change="uploadFile"
+        />
+        上傳歌詞
+      </label>
     </div>
   </div>
 </template>
@@ -23,13 +36,26 @@
 import { ref } from "vue";
 
 const emit = defineEmits(["getLyric", "showError"]);
-const lyric = ref("");
+const lyric = ref(null);
 
 async function sendLyric() {
   if (!lyric.value.length) {
     emit("showError", true);
   } else {
     emit("getLyric", lyric.value);
+  }
+}
+
+async function uploadFile(e) {
+  let file = e.target.files[0];
+  if (!file) return;
+  const reader = new FileReader();
+  reader.addEventListener("load",(e)=>{
+    lyric.value = e.target.result;
+  },false)
+
+  if (file) {
+    reader.readAsText(file);
   }
 }
 </script>

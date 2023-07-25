f3a4958cd2a7928ff3c5694bd599686552a23a01
seesawjack <seesawjack14@gmail.com>
2023-07-25 16:41:29 +0800

    202307251641-調整sidebar的icon寫法

--- src/components/atmos/AtmosSidebar.vue
+++ src/components/atmos/AtmosSidebar.vue
@@ -4,35 +4,26 @@
       <nav class="flex-1 -mx-3 space-y-3">
         <a class="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
           href="/song/search">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-            class="w-5 h-5">
-            <path stroke-linecap="round" stroke-linejoin="round"
-              d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-          </svg>
-
+          <HomePageIcon />
           <span class="mx-2 text-sm font-medium">首頁</span>
         </a>
 
         <a class="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
           href="/song/upload">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-            class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round"
-              d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
-          </svg>
-
-          <span class="mx-2 text-sm font-medium">新增歌曲頁</span>
+          <plus-paper-icon />
+          <span class="mx-2 text-sm font-medium">新增歌曲</span>
         </a>
 
         <a class="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
           href="/song/list">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-            class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round"
-              d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
-          </svg>
+          <paper-icon />
+          <span class="mx-2 text-sm font-medium">個人清單</span>
+        </a>
 
-          <span class="mx-2 text-sm font-medium">已建立歌曲清單</span>
+        <a class="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
+          href="/song/list">
+          <recommend-icon />
+          <span class="mx-2 text-sm font-medium">推薦清單</span>
         </a>
 
         <router-link to="/about"
@@ -74,7 +65,11 @@ import { useAuthStore } from "../../stores/auth";
 import InfoIcon from "../svg/InfoIcon.vue";
 import LogoutIcon from "../svg/LogoutIcon.vue";
 import SigninButton from "../svg/SigninButton.vue";
+import RecommendIcon from "../svg/RecommendIcon.vue";
 import UserIcon from "../svg/UserIcon.vue";
+import PaperIcon from "../svg/PaperIcon.vue";
+import PlusPaperIcon from "../svg/PlusPaperIcon.vue";
+import HomePageIcon from "../svg/HomePageIcon.vue";
 
 const { handleLogout, isLoggedIn } = useAuthStore();
 const { userInfo } = storeToRefs(useAuthStore());

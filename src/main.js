998ba4762451c54e5e647eaf7bfcaa267aa0e3a8
seesawjack <seesawjack14@gmail.com>
2023-08-22 16:51:49 +0800

    202308221650-(1)加入 GA4 追蹤(2)增加 og:image 以及 og:url

--- src/main.js
+++ src/main.js
@@ -3,12 +3,16 @@ import { createPinia } from 'pinia'
 import router from './router'
 import './style.css'
 import App from './App.vue'
+import VueGtag from "vue-gtag";
 
 const pinia = createPinia();
 const app = createApp(App);
 
 app.use(pinia);
 app.use(router);
+app.use(VueGtag, {
+    config: { id: import.meta.env.VITE_GA4_ID }
+},router)
 
 
 app.mount('#app');

1f3ca0ba86fae7dd0ec8374e6c923cc7cbefeac1
seesawjack <seesawjack14@gmail.com>
2023-08-22 17:59:16 +0800

    202308221758-(1)調整 og-image 資料位置及引用方法(2)main.js 寫法調整

--- src/main.js
+++ src/main.js
@@ -6,13 +6,11 @@ import App from './App.vue'
 import VueGtag from "vue-gtag";
 
 const pinia = createPinia();
-const app = createApp(App);
-
-app.use(pinia);
-app.use(router);
-app.use(VueGtag, {
-    config: { id: import.meta.env.VITE_GA4_ID }
-},router)
-
+const app = createApp(App)
+    .use(router)
+    .use(pinia)
+    .use(VueGtag, {
+        config: { id: import.meta.env.VITE_GA4_ID }
+    }, router);
 
 app.mount('#app');

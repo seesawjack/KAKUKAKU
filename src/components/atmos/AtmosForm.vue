89c5c475c2a87affcd5284587ba7cd4b69ae80b4
jiancheng <seesawjack14@gmail.com>
2023-08-23 10:29:38 +0800

    202308231029-修正 select input 在 IOS 裝置上會跑版問題

--- src/components/atmos/AtmosForm.vue
+++ src/components/atmos/AtmosForm.vue
@@ -2,31 +2,58 @@
   <section>
     <div class="container flex items-center justify-center max-md:px-6 mx-auto">
       <Form @submit="formSubmit" class="w-full" :validation-schema="validate">
-        <div v-for="{ as, name, children, icon, icon2, ...attrs } in schema" :key="name" class="relative">
-          <Field :as="as" :id="name" :name="name" v-bind="attrs" :ref="name"
+        <div
+          v-for="{ as, name, children, icon, icon2, ...attrs } in schema"
+          :key="name"
+          class="relative"
+        >
+          <Field
+            :as="as"
+            :id="name"
+            :name="name"
+            v-bind="attrs"
+            :ref="name"
             class="block w-full py-3 border rounded-lg dark:bg-gray-900 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 mt-8"
             :class="{
               'pl-11': name !== 'birth' && icon?.length,
               'pl-3': name === 'birth' || !icon,
-            }">
+            }"
+          >
             <template v-if="children && children.length">
-              <component v-for="({ tag, text, ...childAttrs }, idx) in children" :key="idx" :is="tag" v-bind="childAttrs">
+              <component
+                v-for="({ tag, text, ...childAttrs }, idx) in children"
+                :key="idx"
+                :is="tag"
+                v-bind="childAttrs"
+              >
                 {{ text }}
               </component>
             </template>
           </Field>
-          <component :is="iconGroup[icon]" class="absolute top-3 mx-3 text-gray-300 dark:text-gray-500" />
-          <component v-if="icon2" :is="iconGroup[showPasswordIcon]"
+          <component
+            :is="iconGroup[icon]"
+            class="absolute top-3 mx-3 text-gray-300 dark:text-gray-500"
+          />
+          <component
+            v-if="icon2"
+            :is="iconGroup[showPasswordIcon]"
             class="absolute right-0 top-3 mx-3 text-gray-300 dark:text-gray-500"
-            :class="{ 'stroke-white': showPasswordIcon === 'EyeIcon' }" @click="changIcon" />
-          <ErrorMessage :name="name" class="absolute left-0 top-15 text-sm text-red-500" />
+            :class="{ 'stroke-white': showPasswordIcon === 'EyeIcon' }"
+            @click="changIcon"
+          />
+          <ErrorMessage
+            :name="name"
+            class="absolute left-0 top-15 text-sm text-red-500"
+          />
         </div>
         <button
           class="w-full mt-8 text-md font-medium tracking-wide text-white capitalize transition-colors duration-300 transform border rounded-lg focus:outline-none"
           :class="{
             unclickable: !button.state,
             'hover:bg-sky-900/90': button.state,
-          }" :disabled="!button.state">
+          }"
+          :disabled="!button.state"
+        >
           {{ button.name }}
         </button>
       </Form>
@@ -66,8 +93,8 @@ const props = defineProps({
   },
   formType: {
     type: String,
-    required: true
-  }
+    required: true,
+  },
 });
 
 const formName = ref(props.formType);
@@ -130,6 +157,11 @@ input[type="date"] {
   flex-direction: row-reverse;
   --tw-text-opacity: 1;
   color: rgb(156 163 175 / var(--tw-text-opacity));
+}
+input[type="select"] {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
 }
 
 select {

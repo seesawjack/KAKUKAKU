7ed25c0f28aef3b328102a61bf98d78d279be6ca
seesawjack <seesawjack14@gmail.com>
2023-08-24 14:59:31 +0800

    202308241459-新增網站規範跳窗

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
-  <atmos-loading v-if="isLoading"/>
-  <atmos-dialog
-    :show="errorMessage?.showError"
-    :title="errorMessage?.message"
-    @close="closeDialog"
-  />
+  <atmos-popup v-if="closePopup" :title="popupTitle" :content="popupContent" @agree="handleAgree"
+    @disagree="handleDisagree" />
+  <atmos-loading v-if="isLoading" />
+  <atmos-dialog :show="errorMessage?.showError" :title="errorMessage?.message" @close="closeDialog" />
+
   <the-header />
   <router-view class="max-sm:pt-8 pt-14 max-md:mt-7"></router-view>
   <the-footer />
@@ -12,16 +11,18 @@
 </template>
 
 <script setup>
+import { ref, computed, onMounted, toRefs } from "vue";
+import { useRouter } from "vue-router";
+import { useGlobalStore } from "./stores/index";
+import { useAuthStore } from "./stores/auth";
+import { useRequestStore } from "./stores/request";
+
 import TheHeader from "./components/layouts/TheHeader.vue";
 import BackgroundAnimate from "./components/layouts/BackgroundAnimate.vue";
 import TheFooter from "./components/layouts/TheFooter.vue";
 import AtmosLoading from "./components/atmos/AtmosLoading.vue";
 import AtmosDialog from "./components/atmos/AtmosDialog.vue";
-
-import { onMounted, toRefs } from "vue";
-import { useGlobalStore } from "./stores/index";
-import { useAuthStore } from "./stores/auth";
-import { useRequestStore } from "./stores/request";
+import AtmosPopup from "./components/atmos/AtmosPopup.vue";
 
 const { isLoading } = toRefs(useGlobalStore())
 const { isError } = useGlobalStore();
@@ -31,6 +32,25 @@ const { supabase } = useRequestStore();
 const { userInfo } = toRefs(useAuthStore());
 const { errorMessage } = toRefs(useGlobalStore());
 
+const router = useRouter();
+
+const closePopup = ref(true);
+const popupTitle = "網站使用規範";
+const popupContent = `
+  <ul class="list-disc pl-3">
+    <li>此網站僅供日文學習使用，歌曲和歌詞的版權屬於唱片公司。</li>
+    <li>網站目的在於日文教學，非分享歌曲，喜歡請支持購買原版。</li>
+    <li>請僅在本站學習使用歌曲，切勿透過其他工具下載，本站不提供下載連結。</li>
+  </ul>`
+
+function handleAgree() {
+  closePopup.value = false;
+  localStorage.setItem('cybernorms', true);
+}
+function handleDisagree() {
+  router.back();
+}
+
 function closeDialog() {
   isError({ showError: false, message: "" });
   return;
@@ -39,10 +59,11 @@ function closeDialog() {
 onMounted(() => {
   supabase.auth.onAuthStateChange((event, session) => {
     if (event == "SIGNED_IN") {
-      userInfo.value = session?.user || null;      
+      userInfo.value = session?.user || null;
     }
   });
+
+  closePopup.value = localStorage.getItem('cybernorms') ? false : true;
 });
 </script>
 
-<style></style>
---
+++ src/components/atmos/AtmosPopup.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="absolute w-full h-screen bg-slate-900/70 z-50">
+        <div class="max-w-xl w-full mx-auto translate-y-full rounded-lg bg-white py-8 px-4 shadow-2xl">
+            <h2 class="text-lg font-bold text-gray-950">{{ title }}</h2>
+
+            <p class="mt-2 text-sm text-gray-500 text-justify" v-html="content">
+            </p>
+
+            <div class="mt-4 flex justify-center gap-2">
+                <button type="button" class="rounded bg-green-50 px-4 py-2 text-sm font-medium text-green-600" @click="clickAgree">
+                    {{ button[0] }}
+                </button>
+
+                <button type="button" class="rounded bg-gray-50 px-4 py-2 text-sm font-medium text-gray-600" @click="clickDisagree">
+                    {{ button[1] }}
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    button: {
+        type: Array,
+        default: ["同意", "不同意"]
+    }
+})
+
+const emit = defineEmits(['argee','disagree']);
+function clickAgree(){
+    emit('agree')
+}
+function clickDisagree(){
+    emit('disagree')
+}
+</script>
